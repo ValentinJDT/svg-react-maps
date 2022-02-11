0a75343ce74b2e1bdda353931cf9d03b3e6b5488
ValentinJDT <vjeandot@yahoo.com>
2022-02-11 10:24:31 +0100

    update exports

---
+++ index.js
@@ -0,0 +1,6 @@
+import SVGMap from "./src/components/SVGMap";
+import franceDepartments from "./src/components/maps/franceDepartments.json"
+import franceRegions from "./src/components/maps/franceRegions.json"
+import worldRegions from "./src/components/maps/worldRegions.json"
+
+export {SVGMap, franceRegions, franceDepartments, worldRegions};
